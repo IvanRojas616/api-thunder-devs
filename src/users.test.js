45b93ab8a858312e596404d2266d98ba1827d082
Ivan Rojas <ivanrojas616lost@gmail.com>
2021-10-29 17:25:14 -0500

    Add tiny test with jest

---
+++ src/users.test.js
@@ -0,0 +1,19 @@
+//here we go to test
+
+import queryAllUsers from "./controllers/users/usersController";
+import connectionDB from "./db/connectionDB";
+
+test('should to return one or more users', () => {
+    const callback = (err, res) => {
+        try {
+            expect(res.length).toBeGreaterThan(0);
+            done();
+        } catch (error) {
+            done();
+        }
+    }
+
+    const query = () => queryAllUsers(callback);
+
+    connectionDB.connectDB(query);
+});
